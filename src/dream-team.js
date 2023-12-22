1fa5402f4cec1e44fd22ab94bd358c8be762417f
Koval Anastasiia <ilovepokyshat@gmail.com>
2023-12-22 23:04:53 +0300

    feat: add dream-team fnc

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,30 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  const firstLettersOfNames = [];
+  if (members && members.length > 0) {
+    members.forEach((name) => {
+      if (typeof name === "string") {
+        firstLettersOfNames.push(name.trim()[0].toUpperCase());
+      }
+    });
+  }
+  return firstLettersOfNames.sort().join("");
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
