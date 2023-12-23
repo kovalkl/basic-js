5fe082e8bcba59186cc01ca3581c0059e99e2054
Koval Anastasiia <ilovepokyshat@gmail.com>
2023-12-23 17:54:41 +0300

    feat: add encode-line fnc

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let result = "";
+  let counter = 0;
+
+  function checkPrevLetter(i) {
+    if (str[i] === str[i - 1]) {
+      counter += 1;
+    }
+    if (str[i] !== str[i - 1]) {
+      counter = 1;
+    }
+  }
+
+  // Проверка на пустую строку и цикл
+  for (let i = 0; i < str.length; i += 1) {
+    // Первый символ
+    if (i === 0) {
+      if (str[i] === str[i + 1]) {
+        counter += 1;
+      }
+      if (str[i] !== str[i + 1]) {
+        result += str[i];
+      }
+    }
+
+    // Последующие символы
+    if (i >= 1) {
+      // Далее - конец строки или другой символ
+      if (i === str.length || str[i] !== str[i + 1]) {
+        checkPrevLetter(i);
+
+        if (i < str.length - 1 && counter === 1) {
+          result += str[i];
+          counter = 0;
+        }
+        if (i === str.length - 1 || (i < str.length - 1 && counter > 1)) {
+          if (counter === 1) {
+            result += str[i];
+          }
+          if (counter > 1) {
+            result += `${counter}${str[i]}`;
+          }
+          counter = 0;
+        }
+      }
+
+      // Далее - такой же символ и не конец строки
+      if (i < str.length - 1 && str[i] === str[i + 1]) {
+        checkPrevLetter(i);
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
