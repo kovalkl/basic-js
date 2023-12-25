f4c2253556b33a2faad461d7ba1f386e13fa49c6
Koval Anastasiia <ilovepokyshat@gmail.com>
2023-12-25 15:26:48 +0300

    feat: add matrix-elements-sum fnc

--- src/what-season.js
+++ src/what-season.js
@@ -1,15 +1,15 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
 function getSeason(/* date */) {
   throw new NotImplementedError('Not implemented');
@@ -17,5 +17,5 @@ function getSeason(/* date */) {
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
